08a98ec0a82a78e9ce5480692d325352b2b673fa
Christian-Schneider-Davis <schneiderdavis@aol.com>
2021-03-03 03:51:41 -0600

    react links added

--- basic-routing/src/App.js
+++ basic-routing/src/App.js
@@ -1,15 +1,29 @@
-import logo from './logo.svg';
-import {BrowserRouter, Route, Link} from 'react-router-dom'
+// import logo from './logo.svg';//
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import './App.css';
 
 function Home(props) {
-  return <h1>Home Component</h1>;
+  return <h1>Home Component</h1>; 
+
 }
 function Recipies(props) {
-  return <h1>Recipes Component</h1>;
+  const {push} = props.history;
+  return <>
+  <h1>Recipes Component</h1>
+ 
+  < button onClick={() => push ("/newRecipe")}>Enter New Recipe</button>
+  < button onClick={() => push ("/")}>Return Home</button>
+    </>
 }
 function newRecipe(props) {
-  return <h1>New Recipie Component</h1>;
+  const {push} = props.history;
+  return <>
+  <h1>New Recipe Component</h1>
+  
+  < button onClick={() => push ("/recipes")}>My Recipes</button>
+  < button onClick={() => push ("/")}>Return Home</button>
+    </>
+  
 }
 
 
@@ -18,16 +32,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <BrowserRouter>
-    <Link to = '/home'>Home </Link>
+        <Router>
+    <Link to = '/'>Home </Link>
     <Link to ='/recipes'>My Recipes</Link>
     <Link to= '/newRecipe'> Enter New Recipe </Link>
 
-<Route path='/home' component={Home} /> 
+<Route exact path='/' component={Home} /> 
 <Route path='/recipes' component={Recipies} /> 
 <Route path='/newRecipe' component={newRecipe} /> 
 
-      </BrowserRouter>
+      </Router>
       </header>
     </div>
   );
