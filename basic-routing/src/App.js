cc860c3631555cef994e1fb717a0f2cf0e0ff38a
Christian-Schneider-Davis <schneiderdavis@aol.com>
2021-03-03 04:20:19 -0600

    react links added

--- basic-routing/src/App.js
+++ basic-routing/src/App.js
@@ -1,4 +1,4 @@
-// import logo from './logo.svg';//
+// import logo from './logo.svg';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import './App.css';
 
@@ -18,8 +18,70 @@ function Recipies(props) {
 function newRecipe(props) {
   const {push} = props.history;
   return <>
-  <h1>New Recipe Component</h1>
-  
+  <h1>Enter New Recipe</h1>
+  <div className= "NewEntry">
+
+        <form>
+          <label htmlFor ='dishName'></label>
+          <input
+          maxLength= '20'
+          placeholder= 'Recipe Name'
+          id= 'dishName'
+          name= "dName"
+          type= 'text'
+          />
+        </form>
+
+        <form>
+          <label htmlFor ='dSource'></label>
+          <input
+          maxLength= '20'
+          placeholder= 'Source'
+          id= 'dishSource'
+          name= "dSource"
+          type= 'text'
+          />
+        </form>
+
+        <form>
+          <label htmlFor = 'ingredients'></label>
+          <input
+          maxLength= '100'
+          placeholder= 'Ingredients'
+          id= 'ingredientsLs'
+          name= "ingredLs"
+          type= 'text'
+          />
+        </form>
+
+        <form>
+          <label htmlFor = 'instructions'></label>
+          <input
+          maxLength= '300'
+          placeholder= 'Instructions'
+          id= 'instructions'
+          name= "Instruct"
+          type= 'text'
+          />
+        </form>
+{/* add drop down for category list */}
+         <form>
+           <label htmlFor = 'foodCat'></label>
+          <input
+          maxLength= '10'
+          placeholder= 'Category'
+          id= 'foodCat'
+          name= "foodDropdown"
+          type= 'text'
+          />
+        </form> 
+
+
+
+  </div>
+
+{/*  add functionality to 'Enter New Recipe' button! */}
+  < button onClick={() => push ("/newRecipe")}>Enter New Recipe</button>
   < button onClick={() => push ("/recipes")}>My Recipes</button>
   < button onClick={() => push ("/")}>Return Home</button>
     </>
